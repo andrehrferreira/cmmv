3cb91b59dd8aed9e78ac1073aac34f377a4155bf
Andre <andre@tos.world>
2024-09-04 14:51:00 -0300

    fix headers view

--- .cmmv.config.js
+++ .cmmv.config.js
@@ -42,12 +42,8 @@ module.exports = {
         ],
         link: [
             { rel: 'icon', href: 'assets/favicon/favicon.ico' },
-            { rel: 'apple-touch-icon', sizes: '180x180', href: 'assets/favicon/apple-touch-icon.png' },
-            { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'assets/favicon/favicon-32x32.png' },
-            { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'assets/favicon/favicon-16x16.png' },
-            { rel: 'manifest', href: 'assets/favicon/site.webmanifest' },
-            { rel: "dns-prefetch", href: "http://localhost:3000" },
-            { rel: "preconnect", href: "http://localhost:3000" },     
+            //{ rel: "dns-prefetch", href: "http://localhost:3000" },
+            //{ rel: "preconnect", href: "http://localhost:3000" },     
         ]
     },
 
