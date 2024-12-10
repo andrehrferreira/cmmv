9b7dde5c9e79b85b7535ca63a14b7447ec030117
Andre <andre@tos.world>
2024-12-10 05:29:04 -0300

    fix: descriptions

--- packages/view/view.transpile.ts
+++ packages/view/view.transpile.ts
@@ -9,58 +9,14 @@ export class ViewTranspile implements ITranspile {
     private logger: Logger = new Logger('ViewTranspile');
 
     run(): void {
-        const useVue3 = Config.get<boolean>('view.vue3', false);
         const useTailwind = Config.get<boolean>('view.tailwind', false);
-
-        if (!useVue3) {
-            const outputFileVue3 = path.resolve('public/core/0-vue3.min.js');
-
-            if (fs.existsSync(outputFileVue3)) fs.unlinkSync(outputFileVue3);
-
-            // Reactivity
-            const rootDir = process.cwd();
-            const devFile = path.resolve(
-                __dirname,
-                '../node_modules/@cmmv/reactivity/dist/reactivity.iife.js',
-            );
-            const prodFile = path.resolve(
-                rootDir,
-                './node_modules/@cmmv/reactivity/dist/reactivity.iife.js',
-            );
-
-            if (!fs.existsSync(path.resolve('public/core')))
-                fs.mkdirSync(path.resolve('public/core'), { recursive: true });
-
-            if (fs.existsSync(devFile)) {
-                const reactivity = fs.readFileSync(devFile, 'utf-8');
-                const outputFileReactivity = path.resolve(
-                    'public/core/0-reactivity.min.js',
-                );
-                const minifiedJsReactivity = UglifyJS.minify(reactivity).code;
-                fs.writeFileSync(
-                    outputFileReactivity,
-                    minifiedJsReactivity,
-                    'utf8',
-                );
-            } else if (fs.existsSync(prodFile)) {
-                const reactivity = fs.readFileSync(prodFile, 'utf-8');
-                const outputFileReactivity = path.resolve(
-                    'public/core/0-reactivity.min.js',
-                );
-                const minifiedJsReactivity = UglifyJS.minify(reactivity).code;
-                fs.writeFileSync(
-                    outputFileReactivity,
-                    minifiedJsReactivity,
-                    'utf8',
-                );
-            }
-        }
 
         // Frontend Controller
         const content = fs.readFileSync(
             path.resolve(__dirname, './lib/cmmv.frontend.cjs'),
             'utf-8',
         );
+
         const outputFile = path.resolve('public/core/1-cmmv.min.js');
         const minifiedJsContent = UglifyJS.minify(content).code;
         fs.writeFileSync(outputFile, minifiedJsContent, 'utf8');
