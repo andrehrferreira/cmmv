2981fbb6a386db947e14fd4a626ab002b5521673
Andre <andre@tos.world>
2025-02-14 19:44:00 -0300

    fix: update dependencies

--- packages/http/http.service.ts
+++ packages/http/http.service.ts
@@ -1,65 +1,93 @@
 import { AbstractService, Service } from '@cmmv/core';
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 @Service('http')
 export class HttpService extends AbstractService {
     public name = 'http';
 
-    async request<T = any>(
-        config?: AxiosRequestConfig,
-    ): Promise<AxiosResponse<T>> {
-        const Axios = (await import('axios')).default;
-        return Axios.request<T>(config);
+    private async request<T = any>(
+        url: string,
+        method: string,
+        body?: any,
+        config?: RequestInit,
+    ): Promise<Response> {
+        const headers = {
+            'Content-Type': 'application/json',
+            ...(config?.headers || {}),
+        };
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body: body ? JSON.stringify(body) : undefined,
+            ...config,
+        });
+
+        return response;
     }
 
-    async get<T = any>(
-        url: string,
-        config?: AxiosRequestConfig,
-    ): Promise<AxiosResponse<T>> {
-        const Axios = (await import('axios')).default;
-        return Axios.get<T>(url, config);
+    private async handleResponse<T = any>(response: Response): Promise<T> {
+        const contentType = response.headers.get('content-type');
+        let data: any;
+
+        if (contentType && contentType.includes('application/json')) {
+            data = await response.json();
+        } else {
+            data = await response.text();
+        }
+
+        return {
+            data,
+            status: response.status,
+            statusText: response.statusText,
+            headers: response.headers,
+            config: {},
+        } as any;
     }
 
-    async delete<T = any>(
-        url: string,
-        config?: AxiosRequestConfig,
-    ): Promise<AxiosResponse<T>> {
-        const Axios = (await import('axios')).default;
-        return Axios.delete<T>(url, config);
+    async get<T = any>(url: string, config?: RequestInit): Promise<T> {
+        const response = await this.request<T>(url, 'GET', undefined, config);
+        return this.handleResponse<T>(response);
     }
 
-    async head<T = any>(
-        url: string,
-        config?: AxiosRequestConfig,
-    ): Promise<AxiosResponse<T>> {
-        const Axios = (await import('axios')).default;
-        return Axios.head<T>(url, config);
+    async delete<T = any>(url: string, config?: RequestInit): Promise<T> {
+        const response = await this.request<T>(
+            url,
+            'DELETE',
+            undefined,
+            config,
+        );
+        return this.handleResponse<T>(response);
+    }
+
+    async head<T = any>(url: string, config?: RequestInit): Promise<T> {
+        const response = await this.request<T>(url, 'HEAD', undefined, config);
+        return this.handleResponse<T>(response);
     }
 
     async post<T = any>(
         url: string,
         data?: any,
-        config?: AxiosRequestConfig,
-    ): Promise<AxiosResponse<T>> {
-        const Axios = (await import('axios')).default;
-        return Axios.post<T>(url, data, config);
+        config?: RequestInit,
+    ): Promise<T> {
+        const response = await this.request<T>(url, 'POST', data, config);
+        return this.handleResponse<T>(response);
     }
 
     async put<T = any>(
         url: string,
         data?: any,
-        config?: AxiosRequestConfig,
-    ): Promise<AxiosResponse<T>> {
-        const Axios = (await import('axios')).default;
-        return Axios.put<T>(url, data, config);
+        config?: RequestInit,
+    ): Promise<T> {
+        const response = await this.request<T>(url, 'PUT', data, config);
+        return this.handleResponse<T>(response);
     }
 
     async patch<T = any>(
         url: string,
         data?: any,
-        config?: AxiosRequestConfig,
-    ): Promise<AxiosResponse<T>> {
-        const Axios = (await import('axios')).default;
-        return Axios.patch<T>(url, data, config);
+        config?: RequestInit,
+    ): Promise<T> {
+        const response = await this.request<T>(url, 'PATCH', data, config);
+        return this.handleResponse<T>(response);
     }
 }
